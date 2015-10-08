aad081ede005534a9f15c5aee33b565706ee8bdf
Tibeiru Covaci <tibi@covaci.se>
2015-10-08 14:54:08 +0100

    Added http-client

--- src/AureliaRecipe/wwwroot/src/recipe.js
+++ src/AureliaRecipe/wwwroot/src/recipe.js
@@ -1,3 +1,21 @@
-﻿export class Recipe {
+﻿import {inject} from 'aurelia-framework';
+import {HttpClient} from 'aurelia-http-client';
+
+@inject(HttpClient)
+export class Recipe {
     
+    recipe = null;
+
+    constructor(http){
+        this.http = http;
+    }
+
+    activate(){
+        this.http
+            .get('/data/recipe.json')
+            .then(result => {
+                return this.recipe = result.content;
+            })
+            .catch(console.log);
+    }
 }
